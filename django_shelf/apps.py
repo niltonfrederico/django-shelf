7aa24a5baa45aa53ff5faec74a32fc1a4381abbd
Nilton Teixeira <9078708+niltonfrederico@users.noreply.github.com>
2025-06-16 21:23:11 -0300

    feat(pre-commit): added pre-commit and linted all things up

--- django_shelf/apps.py
+++ django_shelf/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class DjangoShelfConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_shelf'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_shelf"
